3c497733b08169fb5f9eea72506818aa87d128cb
aliefabdillah <aliefmabdillah09@gmail.com>
2023-05-17 22:01:49 +0700

    comment db sync

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -8,12 +8,12 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.e
 });
 
 // Sinkron table pada db dari model
-db.sync({alter: true})
+/* db.sync({alter: true})
   .then(() => {
     console.log('Tabel berhasil di sinkronisasi');
   })
   .catch((error) => {
     console.error('Terjadi kesalahan:', error);
-  });
+  }); */
 
 export default db;
